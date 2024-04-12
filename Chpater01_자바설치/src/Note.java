b125291f31367edd1e8ed5136a0521295a02eb1e
user <user@DESKTOP-VNHSN2U>
2024-04-12 12:55:26 +0900

    0412

---
+++ Chpater01_자바설치/src/Note.java
@@ -0,0 +1,40 @@
+/*
+ * Front-End/Back-End
+ * Front-End : 화면관련
+ * HTML, CSS, JavaScript
+ * Back-End : 요청처리 데이터 관리
+ * Java, Oracle, Spring
+ * 
+ * Front
+ *  ==>퍼블리셔
+ *  ==>개발자
+ *  JavaScript
+ *   ==>Jquery/React/Vue/Nodels/Next.is
+ *   ==>TypeScript
+ *   Back-End+Front-End
+ *    Fullstack
+ *
+ * 
+ * 자바 실행 과정
+ * 
+ * A.java : class 명과 저장명이 동일 ==>class A => 대소문자 구분
+ * A.java ===========>JVM이 인식하는 언어로 변경============>JVM이 처리 ==========>실행
+ *                    ====>바이너리 코드 (0,1)
+ *              ↓                                                    ↓
+ *             javac(컴파일) -> A.class(바이트 코드=>배포)               java
+ *             
+ *                            한줄씩 읽어서 출력 (인터프리터)
+ *                            
+ * .java ==> 프로그래머만 아는 언어(원시 소스)
+ * .class ==> 컴퓨터가 아는 언어 (바이트 코드)
+ * ------------------------------------------이클립스에서는 한번에 처리
+ *                                           CTRL + F11
+ */
+public class Note {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
